b8333f73c0473509713ac282f136a08c601cc8fa
Roway <Roway_luo@163.com>
2018-09-06 20:48:19 +0800

    1

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="D:\workspace\Pycharm\spider\venv"
+$env:VIRTUAL_ENV="D:\workspace\Pycharm\spider\spider\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
--- venv/Scripts/easy_install-script.py
+++ venv/Scripts/easy_install-script.py
@@ -1,4 +1,4 @@
-#!D:\workspace\Pycharm\spider\venv\Scripts\python.exe
+#!D:\workspace\Pycharm\spider\spider\venv\Scripts\python.exe
 # EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==39.1.0','console_scripts','easy_install'
 __requires__ = 'setuptools==39.1.0'
 import re
